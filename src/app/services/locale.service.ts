50ac9dec65eb6acc8f57ea565d2059fe1bf66208
heaven-dev <kah@live.fi>
2021-03-30 12:58:27 +0300

    Version number changed.

--- src/app/services/locale.service.ts
+++ src/app/services/locale.service.ts
@@ -14,7 +14,7 @@ const appName: string = 'Taivas TV7';
 /**
  * Updated by gulp script during a build of the app.
  */
-const appVersion: string = '2.1.5';
+const appVersion: string = '2.1.6';
 
 export const localeFi: string = 'fi';
 export const localeEt: string = 'et';
